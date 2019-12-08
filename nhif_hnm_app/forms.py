f11ce5f7cbd6adeb0868929f72d7ca4c6422c74f
RKNyra <rk.nyra@gmail.com>
2019-12-08 22:52:21 +0300

    customize the placeholder for the review-submission form so as to provide users with a helpful message when sharing their hospital reviews

--- nhif_hnm_app/forms.py
+++ nhif_hnm_app/forms.py
@@ -6,3 +6,6 @@ class ReviewsForm(forms.ModelForm):
     class Meta:
         model = Review
         fields=['reviews']
+        widgets = {
+            'reviews': forms.TextInput(attrs={'placeholder': 'Share your review/experience at this hospital...'})
+        }
